0784f8e0a0287f4136480120033cd0c063f9176a
jbairoso <jbairoso@gmail.com>
2022-09-12 21:49:51 -0700

    about me

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/About.css";
+
+export default function About() {
+  return (
+    <div className="">
+      <div className="">
+        <p>
+        Hello! My name is Jeralyn Airoso. I am currently a Auto Logistics Coordinator. I have decided to challenge myself and strengthen one of my weaknesses, which would be all things related to computers. I am a full-stack web developer student based in California and looking to focus on front end web development.
+        </p>
+      </div>
+      <div className="">
+        <img
+          src=""
+          className=""
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
