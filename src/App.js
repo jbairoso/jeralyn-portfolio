d362525cd7b3da750ab05b02acbc014b9ff00847
jbairoso <jbairoso@gmail.com>
2022-09-12 21:30:26 -0700

    routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="pages">
+        <Header></Header>
+        <Routes>
+          <Route path="/jeralynAiroso/" element={<Home />} />
+          <Route path="/jeralynAiroso/Home" element={<Home />} />
+          <Route path="/jeralynAiroso/About" element={<About />} />
+          <Route path="/jeralynAiroso/Work" element={<Work />} />
+          <Route path="/jeralynAiroso/Contact" element={<Contact />} />
+          <Route path="/jeralynAiroso/Resume" element={<Resume />} />
+        </Routes>
+        <Footer></Footer>
+      </div>
+    </Router>
   );
 }
 
