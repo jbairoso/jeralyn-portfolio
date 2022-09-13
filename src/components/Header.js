b5ca7a26510db53340845175ef05261746564029
jbairoso <jbairoso@gmail.com>
2022-09-12 22:00:06 -0700

    curent page

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import Nav from './Nav'
+import About from './About'
+import Work from './Work'
+import Contact from './Contact'
+import Resume from './Resume'
+import '../styles/Header.css'
+
+
+export default function Header() {
+    const [currentPage, handlePageChange] = useState('Home');
+
+    const renderPage = () => {
+        switch(currentPage) {
+            case "About":
+                return <About></About>
+            case "Work":
+                return <Work></Work>
+            case "Contact":
+                return <Contact></Contact>
+            case "Resume":
+                return <Resume></Resume>
+            
+        }
+    };
