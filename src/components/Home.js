b5d5d58135660cc26e0cff66eaa2cbf2f4032949
jbairoso <jbairoso@gmail.com>
2022-09-12 22:04:23 -0700

    landing page

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Home.css";
+
+export default function Home() {
+  return (
+    <div className="whoAmI">
+      <div>
+        <h3>Hello! My Name is Jeralyn Airoso</h3>
+        <p>I am a Full-Stack Web Developer Student</p>
+      </div>
+    </div>
+  );
+}
