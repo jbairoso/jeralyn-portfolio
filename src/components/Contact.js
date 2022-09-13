c15ab99c78e1473906007d49a7e19d0f3ce89258
jbairoso <jbairoso@gmail.com>
2022-09-12 21:56:10 -0700

    contact me

---
+++ src/components/Contact.js
@@ -0,0 +1,27 @@
+// Body for contact info
+import React from "react";
+import "../styles/Contact.css";
+
+export default function Contact() {
+  return (
+    <div>
+      <h3> Contact me:</h3>
+      <div>
+        <ul>
+          <li>
+            <a href="mailto: jbairoso@gmail.com"></a>
+            <a href="mailto: jbairoso@gmail.com">
+              <p> jbairoso@gmail</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/jbairoso"></a>
+            <a href="https://github.com/jbairoso">
+              <p> jbairoso </p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
