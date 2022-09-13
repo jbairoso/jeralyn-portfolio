ffe9784c4cc94b06e8f4950dd3a4eabf292f95a5
jbairoso <jbairoso@gmail.com>
2022-09-12 22:01:43 -0700

    render

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,33 @@
-import React, { useState } from 'react';
-import Nav from './Nav'
-import About from './About'
-import Work from './Work'
-import Contact from './Contact'
-import Resume from './Resume'
-import '../styles/Header.css'
-
+import React, { useState } from "react";
+import Nav from "./Nav";
+import About from "./About";
+import Work from "./Work";
+import Contact from "./Contact";
+import Resume from "./Resume";
+import "../styles/Header.css";
 
 export default function Header() {
-    const [currentPage, handlePageChange] = useState('Home');
+  const [currentPage, handlePageChange] = useState("Home");
 
-    const renderPage = () => {
-        switch(currentPage) {
-            case "About":
-                return <About></About>
-            case "Work":
-                return <Work></Work>
-            case "Contact":
-                return <Contact></Contact>
-            case "Resume":
-                return <Resume></Resume>
-            
-        }
-    };
+  const renderPage = () => {
+    switch (currentPage) {
+      case "About":
+        return <About></About>;
+      case "Work":
+        return <Work></Work>;
+      case "Contact":
+        return <Contact></Contact>;
+      case "Resume":
+        return <Resume></Resume>;
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      </div>
+      <div>{renderPage()}</div>
+    </div>
+  );
+}
