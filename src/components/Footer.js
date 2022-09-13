a2aebdca5da9954091c848b30601fd945890d523
jbairoso <jbairoso@gmail.com>
2022-09-12 21:57:52 -0700

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        Made with ❣️ by Jeralyn Airoso <span>&copy;</span>2022
+      </p>
+    </div>
+  );
+}
